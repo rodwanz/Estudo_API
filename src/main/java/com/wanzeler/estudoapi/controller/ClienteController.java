732e03d0ed4aef141fe05601ecb6b65a8610fb56
Rodrigo Wanzeler <wanzervai@gmail.com>
2023-01-05 10:57:58 -0300

    Atualizando e excluindo dados do cliente

--- src/main/java/com/wanzeler/estudoapi/controller/ClienteController.java
+++ src/main/java/com/wanzeler/estudoapi/controller/ClienteController.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -52,5 +54,25 @@ public class ClienteController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente adicinar(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
+	}
+	
+	@PutMapping("/{clienteId}")
+	public ResponseEntity<Cliente> atualizando(@PathVariable Long clienteId, 
+			@RequestBody Cliente cliente){
+		if(!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		cliente.setId(clienteId);
+		cliente = clienteRepository.save(cliente);
+		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping("/{clienteId}")
+	public ResponseEntity<Cliente> excluindo(@PathVariable Long clienteId){
+			if(!clienteRepository.existsById(clienteId)) {
+				return ResponseEntity.notFound().build();
+			}
+			clienteRepository.deleteById(clienteId);
+			return ResponseEntity.notFound().build();
 	}
 }
