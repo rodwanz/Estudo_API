fabb6c333e90d2e42ee183fc90b23628df1d87a5
Rodrigo Wanzeler <wanzervai@gmail.com>
2023-01-05 10:19:07 -0300

    Refinando o status de adição

--- src/main/java/com/wanzeler/estudoapi/controller/ClienteController.java
+++ src/main/java/com/wanzeler/estudoapi/controller/ClienteController.java
@@ -3,12 +3,14 @@ package com.wanzeler.estudoapi.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wanzeler.estudoapi.domain.model.Cliente;
@@ -47,6 +49,7 @@ public class ClienteController {
 	}
 	
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente adicinar(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
