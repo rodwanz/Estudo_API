4c2709cfde4941040a5c662468b96f57d824df06
Rodrigo Wanzeler <wanzervai@gmail.com>
2023-01-04 23:08:13 -0300

    Usando Spring Data JPA e customizando metodos

--- src/main/java/com/wanzeler/estudoapi/controller/ClienteController.java
+++ src/main/java/com/wanzeler/estudoapi/controller/ClienteController.java
@@ -5,20 +5,35 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wanzeler.estudoapi.domain.model.Cliente;
+import com.wanzeler.estudoapi.domain.repository.ClienteRepository;
+
+import lombok.AllArgsConstructor;
 
 @RestController
+@AllArgsConstructor
 public class ClienteController {
-	
-	@PersistenceContext
-	private EntityManager manager;
+
+
+	private ClienteRepository clienteRepository;
 	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		return manager.createQuery("from Cliente", Cliente.class)
-				.getResultList();
+		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/clientes/nome")
+	public List<Cliente> listarNome() {
+		return clienteRepository.findByNome("Agostinho Hipona");
+	}
+	
+	@GetMapping("/clientes/nomequalquer")
+	public List<Cliente> listarNomeSemExatidao() {
+		return clienteRepository.findByNomeContaining("Hipona");
 	}
 }
