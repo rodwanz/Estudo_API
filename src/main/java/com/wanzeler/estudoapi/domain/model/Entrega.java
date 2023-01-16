ee96d9d62250d146fce14da1c32778ff69982e68
Rodrigo Wanzeler <wanzervai@gmail.com>
2023-01-16 16:47:44 -0300

    validação em cascata e usando o Validatio Group

--- src/main/java/com/wanzeler/estudoapi/domain/model/Entrega.java
+++ src/main/java/com/wanzeler/estudoapi/domain/model/Entrega.java
@@ -11,16 +11,17 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.groups.ConvertGroup;
+import javax.validation.groups.Default;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
+import lombok.EqualsAndHashCode;
+
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Entrega {
@@ -30,12 +31,18 @@ public class Entrega {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Valid
+	@ConvertGroup(from = Default.class, to = ValidationGroups.ClienteId.class)
+	@NotNull
 	@ManyToOne
 	private Cliente cliente;
 	
+	@Valid
+	@NotNull
 	@Embedded
 	private Destinatario destinatario;
 	
+	@NotNull
 	private BigDecimal taxa;
 	
 	@Enumerated(EnumType.STRING)
@@ -47,6 +54,61 @@ public class Entrega {
 	
 	@JsonProperty(access = Access.READ_ONLY)
 	private LocalDateTime dataFinalizado;
-	
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Destinatario getDestinatario() {
+		return destinatario;
+	}
+
+	public void setDestinatario(Destinatario destinatario) {
+		this.destinatario = destinatario;
+	}
+
+	public BigDecimal getTaxa() {
+		return taxa;
+	}
+
+	public void setTaxa(BigDecimal taxa) {
+		this.taxa = taxa;
+	}
+
+	public StatusEntrega getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusEntrega status) {
+		this.status = status;
+	}
+
+	public LocalDateTime getDataPedido() {
+		return dataPedido;
+	}
+
+	public void setDataPedido(LocalDateTime dataPedido) {
+		this.dataPedido = dataPedido;
+	}
+
+	public LocalDateTime getDataFinalizado() {
+		return dataFinalizado;
+	}
+
+	public void setDataFinalizado(LocalDateTime dataFinalizado) {
+		this.dataFinalizado = dataFinalizado;
+	}
 }
+
---
+++ src/main/java/com/wanzeler/estudoapi/domain/model/ValidationGroups.java
@@ -0,0 +1,7 @@
+package com.wanzeler.estudoapi.domain.model;
+
+public interface ValidationGroups {
+	
+	public interface ClienteId {}
+
+}
